23f3893b2e8c00e25516836b97c8467ea3aeedcb
dhilljr <dhill@whiteboardfederal.com>
2021-07-26 13:12:47 -0400

    restarting

--- starter-api/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE GenericObjects(id int, text varchar(255), done boolean)
-INSERT INTO GenericObjects (id, text, done) VALUES (1, 'Add a third item', false)
-INSERT INTO GenericObjects (id, text, done) VALUES (2, 'Delete the first item', false)
